e04da8233231244b9cfe8a9aaf05c051d3458ed4
Saad Iqbal <saadiqbal.dev@outlook.com>
2024-04-11 20:56:11 +0500

    Zoom in Zoom Out feature

--- src/components/custom/tldraw/Canvas.js
+++ src/components/custom/tldraw/Canvas.js
@@ -4,14 +4,88 @@ import {
   createTLStore,
   defaultShapeUtils,
 } from "@tldraw/tldraw";
+import { useEditor } from "@tldraw/editor";
 import React, { useCallback, useState } from "react";
 import { ActionButton } from "../../ActionButton";
 import { ResponseShapeUtil } from "./ResponseShape";
 import ShareZone from "./ShareZone";
 import { TldrawLogo } from "./TldrawLogo";
 import TopZone from "./TopZone";
+import styled from "styled-components";
 
 const shapeUtils = [ResponseShapeUtil];
+
+const ZoomUI = styled.div`
+  position: fixed;
+  left: 100px;
+  bottom: 0;
+  z-index: 599;
+  pointer-events: all;
+  display: flex;
+  flex-direction: row;
+  background: hsl(204, 16%, 94%);
+  gap: 0.5rem;
+  padding: 5px;
+  border-radius: 13px 13px 0 0;
+  border: 4px solid rgb(249, 250, 251);
+  border-bottom: 0;
+  button {
+    color: #2d2d2d;
+    border: none !important;
+    padding: 6px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    border: transparent;
+    font-size: 12px;
+    gap: 8px;
+    text-shadow: 1px 1px #fff;
+
+    border-radius: 8px !important;
+
+    &:active {
+      border-style: inset;
+      background-color: #d5d5d5;
+      color: #000;
+    }
+
+    &:hover {
+      background-color: #e5e5e5 !important;
+      color: #111 !important;
+    }
+  }
+
+  @media (max-width: 840px) {
+    left: 55px;
+  }
+  @media (max-width: 690px) {
+    display: none;
+  }
+`;
+
+const ZoomIn = () => {
+  const editor = useEditor();
+
+  return (
+    <ZoomUI>
+      <button
+        onClick={() => {
+          editor.zoomIn();
+        }}
+      >
+        <i className="bi bi-plus-lg"></i>
+      </button>
+      <button
+        onClick={() => {
+          editor.zoomOut();
+        }}
+      >
+        <i className="bi bi-dash"></i>
+      </button>
+    </ZoomUI>
+  );
+};
 
 function TldrawCanvas({
   page,
@@ -104,6 +178,7 @@ function TldrawCanvas({
       >
         <ActionButton path={persistance} />
         <TldrawLogo />
+        <ZoomIn />
       </Tldraw>
     </div>
   );
