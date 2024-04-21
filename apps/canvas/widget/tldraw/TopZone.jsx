bc8123780b292d4dbceb53901ed3843ffcc9a829
Saad Iqbal <saadiqbal.dev@outlook.com>
2024-04-22 00:11:27 +0500

    Share Canvas Option in Top Zone

--- apps/canvas/widget/tldraw/TopZone.jsx
+++ apps/canvas/widget/tldraw/TopZone.jsx
@@ -4,8 +4,15 @@ const parts = path.split("/");
 const creatorId = parts[0];
 const thingId = parts[2];
 
+const Button = styled.button`
+  background: transparent;
+  width: 24px;
+  height: 24px;
+  border: none;
+`;
+
 return (
-  <div className="styled-top-bar">
+  <div className="styled-top-bar d-flex align-items-center">
     <Widget
       src="mob.near/widget/N.ProfileLine"
       props={{
@@ -15,5 +22,19 @@ return (
       }}
     />
     {"/" + thingId}
+    <OverlayTrigger
+      placement="auto"
+      overlay={<Tooltip>{"Copy to clipboard"}</Tooltip>}
+    >
+      <Button
+        onClick={() =>
+          clipboard.writeText(
+            `https://draw.everything.dev/${context.accountId}${props.location.search}`
+          )
+        }
+      >
+        <i className="bi bi-share"></i>
+      </Button>
+    </OverlayTrigger>
   </div>
 );
