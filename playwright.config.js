716e7ed5f70b2f9e1b18966f51345b3332dc6ac9
Marpureddy Saswat <saswatmarpureddy@gmail.com>
2024-05-18 13:20:19 +0530

    test-bos-loader

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:4040",
+    baseURL: "http://localhost:8000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -39,12 +39,12 @@ export default defineConfig({
       cookies: [],
       origins: [
         {
-          origin: "http://localhost:4040",
+          origin: "http://localhost:8000",
           localStorage: [
             {
               name: "flags",
               value: JSON.stringify({
-                bosLoaderUrl: "http://127.0.0.1:4040/api/loader",
+                bosLoaderUrl: "http://127.0.0.1:8000/api/loader",
               }),
             },
           ],
@@ -97,7 +97,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    port: 4040,
+    port: 8000,
     reuseExistingServer: !process.env.CI,
   },
 });
